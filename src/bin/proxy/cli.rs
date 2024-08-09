02da49f47f7c49d38bd1d26a4993074dc41c3efd
Paul Brussee <P.W.G.Brussee@tudelft.nl>
2024-08-09 21:26:59 +0200

    chore: upgrade dependency clap to version 4

--- src/bin/proxy/cli.rs
+++ src/bin/proxy/cli.rs
@@ -1,15 +1,15 @@
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[clap(author, version, about)]
+#[derive(Debug, Parser)]
+#[command(about, author, version)]
 /// Proxy for tcp/unix/std to tcp/unix
 pub struct Cli {
-    /// Address of the remote server in (host:port)/Unix socket path to expose local ports to.
-    #[clap{}]
-    pub remote_host: String,
-
-    /// Address of the local server in (host:port)/Unix socket path.
-    ///  if not set, it will read from stdin and write to stdout
-    #[clap{}]
+    /// Address of the local server in `host:port` or Unix socket path.
+    /// If not set, it will read from `stdin` and write to `stdout`.
+    #[arg(short, long, value_name = "LOCAL")]
     pub local_host: Option<String>,
+
+    /// Address of the remote server in `host:port` or Unix socket path to expose local ports to.
+    #[arg(short, long, value_name = "REMOTE")]
+    pub remote_host: String,
 }
